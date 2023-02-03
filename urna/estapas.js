f766bf280c28bceaf2cc53049e6abe66f7fbe78c
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2023-02-03 16:35:49 -0300

    Urna

---
+++ urna/estapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros:5,
+        candidatos:[
+            {
+                numero:7777,
+                nome:'Fulano de tal',
+                partido:'ABC',
+                fotos:[
+                    [
+                        {URL:'7777.jpg', legenda: 'Vereador'}
+                    ]
+                ]
+            },
+
+            {
+                numero:105521,
+                nome:'Beltranio da silva',
+                partido:'ABCD',
+                fotos:[
+                    {URL:'105521', legenda: 'Vereador'}
+                ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:99,
+                nome:'ciclano',
+                partido:'JDF',
+                vice:'Cic',
+                fotos:[
+                    {URL:'99.jpg', legenda:'Prefeito'},
+                    {URL:'99.jpg', legenda: 'Vice-prefeito', small:true}
+                ]
+            },
+
+            {
+                numero:85,
+                nome:'ciclano de tal',
+                partido:'JDF',
+                vice:'Ciclado feio',
+                fotos:[
+                    {URL:'85.jpg', legenda:'Prefeito'},
+                    {URL:'85.jpg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
---
+++ urna/script.js
@@ -0,0 +1,31 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let descriçãoGeral = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let cargo = document.querySelector('.d-1-2 span');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+
+let etapaAtual = 0;
+
+function comecarEtapa(){
+    
+}
+
+
+function insert(n){
+    alert('clicou em ' + n)
+};
+
+
+function branco(){
+
+};
+
+function corrige(){
+
+};
+
+function confirma(){
+
+};
